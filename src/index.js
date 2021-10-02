00bbbe9a8bcc093453a28c11ba5a0913de008f1e
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-02 14:16:44 +0100

    moved to imperial bot backend

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const { SapphireClient } = require('@sapphire/framework');
-const {Intents} = require('discord.js')
-const client = new SapphireClient({ intents: [Intents.FLAGS.GUILDS], prefix: ['*'] });
-client.once('ready', () => {
-    console.log('Ready!')
-});
-client.login(process.env.TOKEN);
