f89840986076c06a8966b08d98d75dbc7e2cdcf4
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-02 15:00:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
+// libs
 require('dotenv').config()
 const token = process.env.token;
 const { SapphireClient } = require('@sapphire/framework');
-const {MessageEmbed} = require('discord.js')
+const {MessageEmbed, ReactionUserManager} = require('discord.js')
 const client = new SapphireClient ({ defaultPrefix: ['*'], intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS']});
 client.once('ready', () => {
 	console.log('Ready!');
@@ -17,5 +17,15 @@ client.once('ready', () => {
 		}
 	]
 	client.guilds.cache.get('893217097809821737')?.commands.set(data);
+});
+// slash commands
+client.on('interactionCreate', interaction => {
+	if (interaction.isCommand()) {
+		if (interaction.commandName === 'ping') {
+			interaction.reply('Pong!');
+		} else if (interaction.commandName === 'help') {
+			interaction.reply('help')
+		}
+	} else return;
 });
 client.login(token);
